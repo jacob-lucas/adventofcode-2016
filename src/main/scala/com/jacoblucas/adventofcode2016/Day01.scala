649f7da5dc457112eaf9edd54dead975059ae767
Jacob Lucas <jacob.lucas@grab.com>
2016-12-02 16:47:15 +0800

    Refactoring to track all visited locations

--- src/main/scala/com/jacoblucas/adventofcode2016/Day01.scala
+++ src/main/scala/com/jacoblucas/adventofcode2016/Day01.scala
@@ -16,43 +16,62 @@ case object West extends Heading
 
 object Day01 {
 
-  def blocksAway(instructions: List[Instruction]): Int = {
-
-    @tailrec
-    def walkToEnd(instructions: List[Instruction], loc: (Int, Int), heading: Heading): (Int, Int) = {
-      instructions match {
-        case x :: xs =>
-//          println("Current loc = " + loc + ", processing: " + x)
-          val (dx, dy, h) = heading match {
-            case North =>
-              x.direction match {
-                case Left => (loc._1 - x.n, loc._2, West)
-                case Right => (loc._1 + x.n, loc._2, East)
-              }
-            case South =>
-              x.direction match {
-                case Left => (loc._1 + x.n, loc._2, East)
-                case Right => (loc._1 - x.n, loc._2, West)
-              }
-            case East =>
-              x.direction match {
-                case Left => (loc._1, loc._2 + x.n, North)
-                case Right => (loc._1, loc._2 - x.n, South)
-              }
-            case West =>
-              x.direction match {
-                case Left => (loc._1, loc._2 - x.n, South)
-                case Right => (loc._1, loc._2 + x.n, North)
-              }
-          }
-          walkToEnd(xs, (dx, dy), h)
-        case Nil => loc
+  def visitedLocations(current: (Int, Int), instruction: Instruction, heading: Heading): (Seq[(Int, Int)], Heading) = {
+    if ((heading == North && instruction.direction == Right) || (heading == South && instruction.direction == Left)) {
+      // along x-axis +ve
+      val locs = for {
+        dx <- current._1 + 1 to current._1 + instruction.n
+      } yield {
+        (dx, current._2)
       }
+      (locs, East)
+    } else if ((heading == North && instruction.direction == Left) || (heading == South && instruction.direction == Right)) {
+      // along x-axis -ve
+      val locs = for {
+        dx <- (current._1 - instruction.n) until current._1
+      } yield {
+        (dx, current._2)
+      }
+      (locs.reverse, West)
+    } else if ((heading == West && instruction.direction == Right) || (heading == East && instruction.direction == Left)) {
+      // along y-axis +ve
+      val locs = for {
+        dy <- current._2 + 1 to current._2 + instruction.n
+      } yield {
+        (current._1, dy)
+      }
+      (locs, North)
+    } else {
+      // along y-axis -ve
+      val locs = for {
+        dy <- (current._2 - instruction.n) until current._2
+      } yield {
+        (current._1, dy)
+      }
+      (locs.reverse, South)
     }
+  }
 
+  @tailrec
+  def walkToEnd(instructions: List[Instruction], locs: List[(Int, Int)], heading: Heading): (Int, Int) = {
+    val loc = locs.last
+    instructions match {
+      case x :: xs =>
+        println("Current loc = " + loc + ", facing: " + heading + ", processing: " + x)
+        val (newLocs, h) = visitedLocations(loc, x, heading)
+        walkToEnd(xs, locs ++ newLocs, h)
+      case Nil => loc
+    }
+  }
+
+  def blocks(x: (Int, Int), y: (Int, Int)): Int = {
+    math.abs(x._1 - y._1) + math.abs(x._2 - y._2)
+  }
+
+  def blocksAway(instructions: List[Instruction]): Int = {
     val start = (0, 0) // could be anywhere
-    val end = walkToEnd(instructions, start, North)
-    math.abs(start._1 - end._1) + math.abs(start._2 - end._2)
+    val end = walkToEnd(instructions, List(start), North)
+    blocks(start, end)
   }
 
   def main(args: Array[String]): Unit = {
@@ -60,7 +79,7 @@ object Day01 {
     val instructions = lines
       .split(",")
       .map(_.trim)
-      .map(s => {
+      .flatMap(s => {
         try {
           val dirStr = s.substring(0, 1)
           val dir = dirStr match {
@@ -77,8 +96,6 @@ object Day01 {
             None
         }
       })
-      .filter(_.nonEmpty)
-      .map(_.get)
       .toList
 
     println(Day01.blocksAway(instructions))
