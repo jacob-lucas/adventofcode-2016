a67faee51ed5be875b0b4796fd9728225028467d
Jacob Lucas <jacob.lucas@grab.com>
2016-12-06 14:01:43 +0800

    Day 6 part 1 complete

---
+++ src/main/scala/com/jacoblucas/adventofcode2016/Day06.scala
@@ -0,0 +1,33 @@
+package com.jacoblucas.adventofcode2016
+
+import scala.annotation.tailrec
+import scala.io.Source
+
+object Day06 {
+
+  @tailrec
+  def errorCorrectMessage(messages: List[String], i: Int, length: Int, msg: String): String = {
+    if (i == length) msg
+    else {
+      val counts = for {
+        m <- messages
+      } yield {
+        (m.charAt(i), messages.count(msg => msg.charAt(i) == m.charAt(i)))
+      }
+      val ch = counts
+        .distinct
+        .sortBy(_._2)
+        .last
+        ._1
+
+      errorCorrectMessage(messages, i + 1, length, msg :+ ch)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val lines = Source.fromInputStream(getClass.getResourceAsStream("/day06-input.txt")).getLines().toList
+    val msg = errorCorrectMessage(lines, 0, lines.head.length, "")
+    println(msg)
+  }
+
+}
