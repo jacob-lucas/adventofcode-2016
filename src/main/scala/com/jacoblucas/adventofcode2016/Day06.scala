2f0c51bbef6375aef44c59286ab9ac2179c82747
Jacob Lucas <jacob.lucas@grab.com>
2016-12-07 09:58:59 +0800

    Day 6 part 2 complete (a little cleaner)

--- src/main/scala/com/jacoblucas/adventofcode2016/Day06.scala
+++ src/main/scala/com/jacoblucas/adventofcode2016/Day06.scala
@@ -6,27 +6,23 @@ import scala.io.Source
 object Day06 {
 
   @tailrec
-  def errorCorrectMessage(messages: List[String], i: Int, length: Int, msg: String): String = {
-    if (i == length) msg
+  def errorCorrectMessage(messages: List[String], i: Int, msg: String): String = {
+    if (i == messages.head.length) msg
     else {
-      val counts = for {
-        m <- messages
-      } yield {
-        (m.charAt(i), messages.count(msg => msg.charAt(i) == m.charAt(i)))
-      }
-      val ch = counts
+      val ch = messages
+        .map(m => (m.charAt(i), messages.count(msg => msg.charAt(i) == m.charAt(i))))
         .distinct
         .sortBy(_._2)
         .head
         ._1
 
-      errorCorrectMessage(messages, i + 1, length, msg :+ ch)
+      errorCorrectMessage(messages, i + 1, msg :+ ch)
     }
   }
 
   def main(args: Array[String]): Unit = {
     val lines = Source.fromInputStream(getClass.getResourceAsStream("/day06-input.txt")).getLines().toList
-    val msg = errorCorrectMessage(lines, 0, lines.head.length, "")
+    val msg = errorCorrectMessage(lines, 0, "")
     println(msg)
   }
 
