31f7ec08cb334fe595d7d2b19b758557ac53be11
Jacob Lucas <jacob.lucas@grab.com>
2016-12-13 08:47:49 +0800

    Day 11 move extracted to case class, skeleton for BFS in place

--- src/main/scala/com/jacoblucas/adventofcode2016/Day11.scala
+++ src/main/scala/com/jacoblucas/adventofcode2016/Day11.scala
@@ -27,8 +27,8 @@ case class Floor(level: Int, materials: mutable.MutableList[Material]) {
   def toString(hasElevator: Boolean): String = "|F" + level + " " + (if (hasElevator) "E " else "  ") + "| " + materials.sortBy(_.toString).mkString(" ")
 }
 
-case class Facility(floors: mutable.MutableList[Floor], elevator: Elevator) {
-  def safeMove(from: Floor, to: Floor, materials: List[Material]): Boolean = {
+case class Move(from: Floor, to:Floor, materials: List[Material]) {
+  def safe: Boolean = {
     val remaining = from.materials.filterNot(m => materials.contains(m))
     val adjusted = to.materials ++ materials
 
@@ -48,24 +48,35 @@ case class Facility(floors: mutable.MutableList[Floor], elevator: Elevator) {
     safe(remaining.toList) && safe(adjusted.toList)
   }
 
-  def move(from: Floor, to:Floor, materials: List[Material]) = {
-    if (!(floors.map(_.level).contains(from.level) && floors.map(_.level).contains(to.level))) {
-      println("WARN: facility does not contain floor(s): " + from.level + ", " + to.level)
-    } else if (!materials.forall(from.materials.contains(_))) {
-      println("WARN: Floor " + from + " does not contain all requested materials: " + materials)
-    } else if (elevator.level != from.level) {
-      println("WARN: Elevator not at level: " + from.level)
-    } else if (materials.length < elevator.minCapacity || materials.length > elevator.maxCapacity) {
-      println("WARN: Elevator capacity constraints not met to move " + materials + " from L" + from.level + " to L" + to.level)
-    } else if (!safeMove(from, to, materials)) {
-      println("WARN: Unsafe to move " + materials + " from L" + from.level + " to L" + to.level)
+  override def toString: String = "Moved " + materials.mkString(",") + " from L" + from.level + " to L" + to.level
+}
+
+case class Facility(floors: mutable.MutableList[Floor], elevator: Elevator) {
+  var visited = false
+  def setVisited(b: Boolean) = {
+    visited = b
+  }
+
+  def score: Int = floors.map(f => f.level * f.materials.length).sum
+
+  def move(m: Move) = {
+    if (!(floors.map(_.level).contains(m.from.level) && floors.map(_.level).contains(m.to.level))) {
+      println("WARN: facility does not contain floor(s): " + m.from.level + ", " + m.to.level)
+    } else if (!m.materials.forall(m.from.materials.contains(_))) {
+      println("WARN: Floor " + m.from + " does not contain all requested materials: " + m.materials)
+    } else if (elevator.level != m.from.level) {
+      println("WARN: Elevator not at level: " + m.from.level)
+    } else if (m.materials.length < elevator.minCapacity || m.materials.length > elevator.maxCapacity) {
+      println("WARN: Elevator capacity constraints not met to move " + m.materials + " from L" + m.from.level + " to L" + m.to.level)
+    } else if (!m.safe) {
+      println("WARN: Unsafe to move " + m.materials + " from L" + m.from.level + " to L" + m.to.level)
     } else {
-      materials.foreach(m => {
-        from.remove(m)
-        to.add(m)
-        elevator.setLevel(to.level)
-        println("Moved " + m + " from L" + from.level + " to L" + to.level)
+      m.materials.foreach(mo => {
+        m.from.remove(mo)
+        m.to.add(mo)
+        elevator.setLevel(m.to.level)
       })
+      println(m)
       println(this)
     }
   }
@@ -105,17 +116,34 @@ object Day11 {
     val f4: Floor = Floor(4, mutable.MutableList())
     val facility = Facility(mutable.MutableList(f1, f2, f3, f4), new Elevator(1, 1, 2))
 
-    println(facility)
-    facility.move(f1, f2, List(hydrogen))
-    facility.move(f2, f3, List(hydrogen, hydrogenGen))
-    facility.move(f3, f2, List(hydrogen))
-    facility.move(f2, f1, List(hydrogen))
-    facility.move(f1, f2, List(hydrogen, lithium))
-    facility.move(f2, f3, List(hydrogen, lithium))
-    facility.move(f3, f4, List(hydrogen, lithium))
-    facility.move(f4, f3, List(hydrogen))
-    facility.move(f3, f4, List(hydrogenGen, lithiumGen))
-    facility.move(f4, f3, List(lithium))
-    facility.move(f3, f4, List(hydrogen, lithium))
+    facility.move(Move(f1, f2, List(hydrogen)))
+    facility.move(Move(f2, f3, List(hydrogen, hydrogenGen)))
+    facility.move(Move(f3, f2, List(hydrogen)))
+    facility.move(Move(f2, f1, List(hydrogen)))
+    facility.move(Move(f1, f2, List(hydrogen, lithium)))
+    facility.move(Move(f2, f3, List(hydrogen, lithium)))
+    facility.move(Move(f3, f4, List(hydrogen, lithium)))
+    facility.move(Move(f4, f3, List(hydrogen)))
+    facility.move(Move(f3, f4, List(hydrogenGen, lithiumGen)))
+    facility.move(Move(f4, f3, List(lithium)))
+    facility.move(Move(f3, f4, List(hydrogen, lithium)))
+
+    val queue = new mutable.Queue[Facility]()
+    queue.enqueue(facility)
+    while (queue.nonEmpty) {
+      val f = queue.dequeue()
+      if (!f.visited) {
+        if (f.score == f.floors.length * f.floors.map(_.materials.length).sum) {
+          // win
+          println("WIN!")
+          queue.clear()
+        } else {
+          // create new facilities based on f
+          // add to queue
+          f.setVisited(true)
+          println(f)
+        }
+      }
+    }
   }
 }
